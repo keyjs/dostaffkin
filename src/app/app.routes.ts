1622b36f82b9eb6d335fe21e2bdb1972824996cc
Key <82021341+keyjs@users.noreply.github.com>
2026-03-19 20:28:59 +0000

    project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Order } from './pages/order/order';
+import { Home } from './pages/home/home';
+import { Track } from './pages/track/track';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'order',
+    component: Order
+  },
+  {
+    path: 'track',
+    component: Track
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
